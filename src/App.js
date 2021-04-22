a028b9bd10249627769272723e7e79006d55e9de
lutful bari <lutfulbarimusa@gmail.com>
2021-04-22 14:40:30 +0600

    Reacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Book from './components/Book';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    books: [
+      { bookName: "1994", writer: "Gerge Orwell" },
+      { bookName: "The DA Vinci Code", writer: "Dan Brown" },
+      { bookName: "THE Alchemist", writer: "Paulo coelho" },
+    ],
+    otherProp: "I am some other prop"
+  }
+
+  changeBookState = newBookName => {
+    this.setState({
+      books: [
+        { bookName: newBookName, writer: "Gerge Orwell" },
+        { bookName: "The DA Vinci Code", writer: "Dan Brown" },
+        { bookName: "MUSA", writer: "lutful bati" },
+      ]
+    });
+  }
+
+  ChangeWithInputState = event => {
+    this.setState({
+      books: [
+        { bookName: event.target.value, writer: "Gerge Orwell" },
+        { bookName: "The DA Vinci Code", writer: "Dan Brown" },
+        { bookName: "MUSA", writer: "lutful bati" },
+      ]
+    });
+  }
+
+  render() {
+    const style = {
+      border: "1px solid red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "white",
+    };
+    console.log(this.state);
+    return (
+      <div className="App">
+        <h1 style={style}>Book list</h1>
+        <button onClick={() => this.changeBookState("Ninteen Eighty-Four")}>Change state</button>
+        <input type="text" onChange={this.ChangeWithInputState} />
+        <Book bookName={this.state.books[0].bookName} writer={this.state.books[0].writer} inputName={this.ChangeWithInputState} />
+        <Book bookName={this.state.books[1].bookName} writer={this.state.books[1].writer} />
+        <Book bookName={this.state.books[2].bookName} writer={this.state.books[2].writer} change={this.changeBookState.bind(this, "Ninteen 84")} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
